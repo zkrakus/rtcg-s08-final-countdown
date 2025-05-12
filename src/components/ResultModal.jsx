c5e090c8a07b22ee0d3e5be036116c652709c3d0
zkrakus <zkrakus@gmail.com>
2025-05-12 13:38:14 -0400

    feat:s08-l142 Cretated ResultModal component that takes a ref to control visibility.

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,14 @@
+import { forwardRef} from 'react'
+
+const ResultModal = forwardRef(function ResultModal({ result, targetTime}, ref){
+    return (<dialog  ref={ref} className="result-modal">
+        <h2>You {result}</h2>
+        <p>The target time was <strong>{targetTime} seconds.</strong></p>
+        <p>The stopped the timer with <strong>X seconds left.</strong></p>
+        <form method="dialog">
+            <button>Close</button>
+        </form>
+    </dialog>);
+});
+
+export default ResultModal;
